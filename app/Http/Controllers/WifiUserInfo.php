f46695f3ae36c310bdb9fbebd0eb3c3d0477c1bb
ed-santos <esantos@live.ca>
2019-03-19 19:51:38 -0400

    url fix

--- app/Http/Controllers/WifiUserInfo.php
+++ app/Http/Controllers/WifiUserInfo.php
@@ -10,9 +10,26 @@ class WifiUserInfo extends Controller
     //
 
     public function receiveData(){
-        return 'hello';
+        $userData = $_POST['client'];
+
+        $name = $element['name'];
+        $email = $element['email'];
+        $provider = $element['provider'];
+        $locationid = $element['loctionid'];
 
 
+
+        //Create new object to pass data to DB
+        $wifiUsers = new wifiUser();
+        //objects to pass to table attributes with assigned declared values above
+        $wifiUsers->fname = $name;
+        $wifiUsers->email = $email;
+        $wifiUsers->provider = $provider;
+        $wifiUsers->locationid = $locationid;
+
+        $wifiUsers-> save();
+
+        //return redirect('/home');
     }
 
 }
