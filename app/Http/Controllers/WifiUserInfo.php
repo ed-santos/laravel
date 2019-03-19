33425d3a65c74a343fbcc7f16958e4726b9bfbed
ed-santos <esantos@live.ca>
2019-03-19 19:45:44 -0400

    url fix

--- app/Http/Controllers/WifiUserInfo.php
+++ app/Http/Controllers/WifiUserInfo.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\wifiUser;
 use Illuminate\Http\Request;
 
 class WifiUserInfo extends Controller
@@ -10,26 +11,7 @@ class WifiUserInfo extends Controller
 
     public function receiveData(){
         return 'hello';
-        $userData = $_POST['client'];
-
-        $name = $element['name'];
-        $email = $element['email'];
-        $provider = $element['provider'];
-        $locationid = $element['loctionid'];
-
-
-
-        //Create new object to pass data to DB
-        $wifiUsers = new Data();
-        //objects to pass to table attributes with assigned declared values above
-        $wifiUsers->fname = $name;
-        $wifiUsers->email = $email;
-        $wifiUsers->provider = $provider;
-        $wifiUsers->locationid = $locationid;
-
-        $wifiUsers-> save();
-
-        return redirect('/home');
+        
 
     }
 
